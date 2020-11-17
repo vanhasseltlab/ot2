944b21714160573c2fda16df0f49e47689593f11
sebastian tandar <sebastian.tandar@gmail.com>
2020-11-17 12:33:25 +0100

    Update server.R
    fixed directory for input template

--- SingleplateMIC/server.R
+++ SingleplateMIC/server.R
@@ -906,7 +906,7 @@ shinyServer(function(input, output) {
   #defining directory-------
   outputDir_cmdline <- "/srv/shiny-server/files/Output_CmdList"
   outputDir_usrGuide <- "/srv/shiny-server/files/Output_UsrGuide"
-  inputTemplate <- "/srv/shiny-server/MIC_Uploader_vx5/MIC_InputTemplate.xlsx"  
+  inputTemplate <- "/srv/shiny-server/ot2/SingleplateMIC/MIC_InputTemplate.xlsx"  
   
   #outputDir_cmdline <- "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\#OT2_Main\\SingleplateMIC"
   #outputDir_usrGuide <- "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\#OT2_Main\\SingleplateMIC"
