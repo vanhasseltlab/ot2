7ebd3dc667d0c3be6a56494629fa5600397bd4da
sebastian tandar <sebastian.tandar@gmail.com>
2020-12-11 23:30:36 +0100

    Create MVPlate controller

---
+++ MVPlate/ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+shinyUI(
+  pageWithSidebar(
+    headerPanel("Multiplate_VariableConcentration - OT2-Commander -- TrialVersion"),
+    
+    sidebarPanel(
+      tags$a(href="http://132.229.100.197:2222/ot2/SingleplateMIC/",
+             "Link to Single Plate MIC"),
+      fileInput("file", "Upload Plate Map", accept=".xlsx"),
+      downloadButton("downloadTemplate", label = "Template Input"),
+      textInput("pmid", 'Plate Map ID (PMID)'),
+      textInput("f_name", 'First Name'),
+      textInput("l_name", 'Last Name'),
+      textInput("exp_name", 'Experiment Name'),
+      textInput("exp_num", 'Experiment Number'),
+      textOutput('tex'),
+      actionButton("do", "Confirm uploaded file and save"),
+      uiOutput('downloadData'),
+      uiOutput('downloadData2'),
+      uiOutput('downloadData3'),
+      
+      tags$a(href="http://132.229.100.197:2222/OldPatch/SingleplateMIC/",
+             "Link to last working uploader")
+    ),
+    mainPanel(
+      tableOutput('tab')
+    )
+))
