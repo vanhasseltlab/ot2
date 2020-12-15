91d3756a9d25107195058ae90e68f0299d8da432
sebastian tandar <sebastian.tandar@gmail.com>
2020-12-15 17:10:00 +0100

    update

---
+++ SeparatedCommaned.py
@@ -0,0 +1,40 @@
+fileName = input("File Name: ")
+
+#IMPORTS---------
+import os
+import opentrons.execute
+sourceLoc = "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\ot2"
+#sourceLoc = '/var/lib/jupyter/notebooks/User Inputs'
+os.chdir(sourceLoc)
+from OT2_ControllerLib import *
+from opentrons import simulate
+
+#READING COMMAND LIST----------
+save_folder = "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\ot2"
+#save_folder = '/var/lib/jupyter/notebooks/User Inputs'
+os.chdir(save_folder)
+amtList, cmdList, deckMap = ReadCSV_Dat(fileName)
+
+########### SIMULATE ############
+#define protocol
+bep = simulate.get_protocol_api('2.3')
+
+#Operation
+bep.home()
+try:
+    run(bep, cmdList, deckMap, amtList) 
+    print("\n=====================================================\nSIMULATION COMPLETE -- EVERYTHING SEEMS OKAY (so far)\n=====================================================")
+except:
+    print("Run terminated prematurely (see the last printed step)")
+
+#prompt decision
+continue_dec = input("Continue run? (Y/N)")
+
+########### EXECUTE ############
+if(continue_dec == "Y" or continue_dec=="y"):
+    #define protocol
+    bep = opentrons.execute.get_protocol_api('2.3')
+
+    #Operation
+    bep.home()
+    run(bep, cmdList, deckMap, amtList) 
