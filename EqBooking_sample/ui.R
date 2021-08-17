c39649492ccdba48e49cb78111b2f63ab38c8de0
sebastian tandar <sebastian.tandar@gmail.com>
2021-08-17 16:18:47 +0200

    troubleshooting_path3

--- EqBooking_sample/ui.R
+++ EqBooking_sample/ui.R
@@ -1,3 +1,22 @@
+#INPUT-------------
+#mainDir <- "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\Incubator\\Calendar\\EqBooking_v5"
+mainDir <- "/srv/shiny-server/ot2/EqBooking_sample"
+
+scheduleTable_dir <- "ScheduleHardCopy.xlsx"
+
+#LIBRARIES-------
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(readxl)
+library(writexl)
+library(chron)
+library(reshape2)
+library(shinyjs)
+library(scrypt)
+source(paste0(mainDir, "\\CalendarSetup.R"))
+scheduleTable <- read_excel(paste0(mainDir, "\\", scheduleTable_dir), sheet=1)
+
 #MAIN----------
 shinyUI(fluidPage(
     
