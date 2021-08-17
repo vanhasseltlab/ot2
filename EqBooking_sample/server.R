5485195fc0a7c78faeeb06ff7464ffd0183dd00f
sebastian tandar <sebastian.tandar@gmail.com>
2021-08-17 16:30:55 +0200

    Update server.R

--- EqBooking_sample/server.R
+++ EqBooking_sample/server.R
@@ -1,3 +1,23 @@
+#INPUT-------------
+#mainDir <- "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\Incubator\\Calendar\\EqBooking_v5"
+mainDir <- "/srv/shiny-server/ot2/EqBooking_sample"
+scheduleTable_dir <- "ScheduleHardCopy.xlsx"
+userLog_dir <- "sneakyLogin.csv"
+
+#LIBRARIES-------------
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(readxl)
+library(writexl)
+library(chron)
+library(reshape2)
+library(shinyjs)
+#library(scrypt)
+require("V8")
+source(paste0(mainDir, "\\CalendarSetup.R"))
+
+
 #MAIN-------
 shinyServer(function(input, output) {
     
