1602652dd55782e96fc5e0bf148a737fb2d62f42
sebastian-tandar <sebastian.tandar@gmail.com>
2021-08-23 15:00:14 +0200

    remove troubleshoot marker

--- EqBooking_sample/server.R
+++ EqBooking_sample/server.R
@@ -2,12 +2,12 @@
 #mainDir <- "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\Incubator"
 #mainDir2 <- "C:\\Users\\Sebastian\\Desktop\\MSc Leiden 2nd Year\\##LabAst Works\\Incubator\\EqBooking_sample"
 
-mainDir <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\EquipmentBook_CvH"
-mainDir2 <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\EquipmentBook_CvH\\main"
+#mainDir <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\EquipmentBook_CvH"
+#mainDir2 <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\EquipmentBook_CvH\\main"
 
 #webserver inputs
-#mainDir <- "/srv/shiny-server/files/EqBooking"
-#mainDir2 <- "/srv/shiny-server/ot2/EqBooking_sample"
+mainDir <- "/srv/shiny-server/files/EqBooking"
+mainDir2 <- "/srv/shiny-server/ot2/EqBooking_sample"
 
 scheduleTable_dir <- "ScheduleHardCopy.xlsx"
 userLog_dir <- "sneakyLogin.csv"
@@ -22,8 +22,8 @@ library(chron)
 library(reshape2)
 library(shinyjs)
 library(stringi)
-#library(scrypt, lib.loc="/home/sebastian/R/x86_64-pc-linux-gnu-library/4.1/")
-library(scrypt)
+library(scrypt, lib.loc="/home/sebastian/R/x86_64-pc-linux-gnu-library/4.1/")
+#library(scrypt)
 source(paste0(mainDir2, "/CalendarSetup.R"))
 
 #MAIN-------
