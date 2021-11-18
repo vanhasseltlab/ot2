7f8ece0232335a6be4bc3e5aa9f05f4f6969be0a
sebastian-tandar <sebastian.tandar@gmail.com>
2021-11-18 10:43:34 +0100

    Check source call

--- Plate384/server.R
+++ Plate384/server.R
@@ -11,104 +11,18 @@ options(stringsAsFactors = F)
 #SERVER MAIN------------
 shinyServer(function(input, output) {
   #defining directory-------
-  #sourceDir <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384\\lauraPlate_processor.R"
-  #outputDir_cmdline <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384"
-  #outputDir_usrGuide <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384"
-  #inputTemplate <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384\\20211111_384TemplateInput.xlsx"
+  sourceDir <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384\\lauraPlate_processor.R"
+  outputDir_cmdline <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384"
+  outputDir_usrGuide <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384"
+  inputTemplate <- "C:\\Users\\sebas\\OneDrive\\Documents\\WebServer\\Incubator\\LauraPlate\\Plate384\\20211111_384TemplateInput.xlsx"
   
-  outputDir_cmdline <- "/srv/shiny-server/files/Output_CmdList"
-  outputDir_usrGuide <- "/srv/shiny-server/files/Output_UsrGuide"
-  inputTemplate <- "/srv/shiny-server/ot2/Plate384/20211111_384TemplateInput.xlsx" 
-  sourceDir <- "/srv/shiny-server/ot2/Plate384/lauraPlate_processor.R" 
+  #outputDir_cmdline <- "/srv/shiny-server/files/Output_CmdList"
+  #outputDir_usrGuide <- "/srv/shiny-server/files/Output_UsrGuide"
+  #inputTemplate <- "/srv/shiny-server/ot2/Plate384/20211111_384TemplateInput.xlsx" 
+  #sourceDir <- "/srv/shiny-server/ot2/Plate384/lauraPlate_processor.R" 
   
   #loading functions--------
   source(sourceDir)
   
-  #initiating error message
-  errMessage <<- ""
   
-  #Obtain names---------
-  new_name <- reactive({
-    if(is.null(input$pmid)){pmid <- ''}else{pmid <- input$pmid}
-    if(is.null(input$exp_name)){expName <- ''}else{expName <- input$exp_name}
-    if(is.null(input$exp_num)){expNum <- ''}else{expNum <- input$exp_num}
-    if(is.null(input$f_name)){fName <- ''}else{fName <- input$f_name}
-    if(is.null(input$l_name)){lName <- ''}else{lName <- input$l_name}
-    
-    #paste
-    res <- paste("PMID-", pmid, "_EXPID-", 
-                 expName, "-", expNum, "_", lName, ".", fName, sep='')
-    
-    return(res)
-  })
-  
-  #Confirming Upload File-----------
-  contents <- reactive({
-    infile <- input$file
-    if(is.null(infile)){return(NULL)}
-    
-    if(input$do==0){
-      # for show
-      res <- read_xlsx(infile$datapath, sheet=2, range = "A59:Y75")
-    }else{
-      res <- mainExec(infile$datapath)
-      cmdList_output <<- res[[1]]
-      usrGuide_output <<- res[[2]]
-      
-      #savekeeping output files
-      #command line
-      cmdLine_name <- paste("CommandList_", new_name(), '.csv', sep='')
-      write_dir <- paste(outputDir_cmdline, cmdLine_name, sep='/')
-      write.csv(cmdList_output, write_dir, row.names = FALSE)
-      
-      #user guide
-      usrGuide_name <- paste("RobotHandler_", new_name(), '.xlsx', sep='')
-      write_dir <- paste(outputDir_usrGuide, usrGuide_name, sep='/')
-      write_xlsx(usrGuide_output, write_dir, col_names=T)
-      
-      # for show in table
-      res <- usrGuide_output[[2]]
-    }
-    
-    return(res)
-  })
-  
-  #DOWNLOAD INPUT TEMPLATE-------
-  output$downloadTemplate <- downloadHandler(
-    filename = "384Well_InputTemplate.xlsx",
-    content = function(file) {
-      file.copy(inputTemplate, file)
-    }
-  )
-  
-  
-  #Sample File Name--------
-  output$tex <- renderText({new_name()})
-  
-  #TABLE FOR SHOW---------
-  output$tab <- renderTable({contents()})
-  
-  #Enabling download button-------
-  output$downloadData <- renderUI({
-    req(input$do, contents())
-    downloadButton("d_OT2", "Download Robot Commands")
-  })
-  output$downloadData2 <- renderUI({
-    req(input$do, contents())
-    downloadButton("guide", "Download Robot Setup Guide")
-  })
-  
-  #Defining download buttons--------
-  output$d_OT2 <- downloadHandler(
-    filename = function(){paste("CommandList_", new_name(), '.csv', sep='')},
-    content = function(file) {
-      write.csv(cmdList_output, file, row.names = FALSE)
-    }
-  )
-  output$guide <- downloadHandler(
-    filename = function(){paste("RobotHandler_", new_name(), '.xlsx', sep='')},
-    content = function(file) {
-      write_xlsx(usrGuide_output, file)
-    }
-  )
 })
