6b68c9eddc125871e489a8bf55c64e6332ad0b19
sebastian-tandar <sebastian.tandar@gmail.com>
2022-09-14 14:56:12 +0200

    384Template and home path

--- home/ui.R
+++ home/ui.R
@@ -9,36 +9,36 @@ shinyUI(fluidPage(
             #OT2 protocol designers
             titlePanel("Protocol Designer"),
             tipify(actionButton("singleplateMIC", "MIC : \tSingleplate", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/SingleplateMIC')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/SingleplateMIC')"),
                    "OT2 protocol processor for MIC with one plate", placement='right', trigger='hover'),
             
             tipify(actionButton("MultiplateMIC", "MIC : \tMultiplate", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/MVPlate')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/MVPlate')"),
                    "OT2 protocol processor for MIC with multiple plates", placement='right', trigger='hover'),
             
             tipify(actionButton("CheckerboardMIC", "MIC : \tCombination | Checkerboard", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/CQ_Plate')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/CQ_Plate')"),
                    "OT2 protocol processor for MIC with combination drugs (applicable for multiple plates)", placement='right', trigger='hover'),
             
             tipify(actionButton("384WellPlateMIC", "MIC : \tMultiplate | 384-well", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/Plate384')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/Plate384')"),
                    "OT2 protocol processor for MIC using 384-well plates (applicable for multiple plates)", placement='right', trigger='hover'),
             
             tipify(actionButton("48WellPlate", "DirectedEvolution: Multiplate | 48-well", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/Plate48')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/Plate48')"),
                    "OT2 protocol processor for 48-well culture plates (applicable for up to 3 plates)", placement='right', trigger='hover'),
             
             tipify(actionButton("M9Mixer", "Medium Mixer", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/M9MixR')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/M9MixR')"),
                    "OT2 protocol processor for medium/solution mixing", placement='right', trigger='hover'),
             
             #plate analysis
             titlePanel("Output Preprocessor"),
             tipify(actionButton("growthCurve", "Regular Growth Data", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/PlateAnalysis/GrowthCurve')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/PlateAnalysis/GrowthCurve')"),
                    "Integrates plate reader outputs into a long-format table", placement='right', trigger='hover'),
             tipify(actionButton("growthCurve", "Checkerboard Growth Data", width='300px',
-                                onclick ="window.open('https://vanhasseltlab.lacdr.leidenuniv.nl/ot2/PlateAnalysis/Checkerboard_DataReader')"),
+                                onclick ="window.open('https://ot2.lacdr.leidenuniv.nl/ot2/PlateAnalysis/Checkerboard_DataReader')"),
                    "Integrates checkerboard plate reader outputs into a long-format table", placement='right', trigger='hover'),
             
             #User Guide download buttons
