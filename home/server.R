db44285687b5ecd52a2f836fbf488b2907771093
sebastian-tandar <sebastian.tandar@gmail.com>
2022-09-14 14:33:13 +0200

    home check

--- home/server.R
+++ home/server.R
@@ -1,8 +1,8 @@
 shinyServer(function(input, output) {
   #setup directories
-  robotGuideDir <- "/srv/shiny-server/ot2/home/2021-03-05_OT2 General Guideline and Maintenance.pdf" 
-  robotQuickGuideDir <- "/srv/shiny-server/ot2/home/2021-08-31_OT2QuickGuide.pdf"
-  webserverGuideDir <- "/srv/shiny-server/ot2/home/2021-07-23_WebserverGuide.pdf" 
+  robotGuideDir <- "/home/shiny-ot2/ShinyApps/ot2/home/2021-03-05_OT2 General Guideline and Maintenance.pdf" 
+  robotQuickGuideDir <- "/home/shiny-ot2/ShinyApps/ot2/home/2021-08-31_OT2QuickGuide.pdf"
+  webserverGuideDir <- "/home/shiny-ot2/ShinyApps/ot2/home/2021-07-23_WebserverGuide.pdf" 
   
   output$downloadRobotGuide <- downloadHandler(
     filename="2021-03-05_OT2 General Guideline and Maintenance.pdf",
