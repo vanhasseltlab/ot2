b36cb0f380028c9e275712f7dea58cb0b2e9e2a3
sebastian-tandar <71292919+sebastian-tandar@users.noreply.github.com>
2023-12-11 17:11:34 +0100

    Update Directscriptmaker.py
    Collapsed filesend functions

--- OT2DirectprotocolCustomizer/V12/Directscriptmaker.py
+++ OT2DirectprotocolCustomizer/V12/Directscriptmaker.py
@@ -8,6 +8,9 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 from pathlib import Path
 from urllib.request import urlopen
@@ -74,11 +77,20 @@ def Webdriver():
         ]
     return sg.Window("Webdriver", layout, finalize= True)
 
-#File sending function Both this one and the other 384: there be dragons
-def Filesending(fullpath, pmid_plate, Firstname, Lastname, Experiment_name, Experiment_num, simulation):
+#File sending function
+def Filesending(fullpath, fillingrobot, notfillingrobot, 
+                pmid_plate, Firstname, Lastname, Experiment_name, 
+                Experiment_num, simulation, mode_384 = False):
     x = 'NA'
     
-    #This part searches ID of the HTML and adds the variables to it
+    if(mode_384):
+        if (fillingrobot == True):
+            element = driver.find_element(By.ID, "fillOuter")
+            element.click()
+        else:
+            print("no outerwell will be filled by OT") 
+    
+    # grab and send value name for webserver input
     driver.find_element(By.ID, "file").send_keys(fullpath)
     driver.find_element(By.ID, "pmid").send_keys(pmid_plate)
     driver.find_element(By.ID, "f_name").send_keys(Firstname)
@@ -86,112 +98,26 @@ def Filesending(fullpath, pmid_plate, Firstname, Lastname, Experiment_name, Expe
     driver.find_element(By.ID, "exp_name").send_keys(Experiment_name)
     driver.find_element(By.ID, "exp_num").send_keys(Experiment_num)
     
-    #Sleep timers to not try to click the download button before server is ready
-    time.sleep(0.5)
+    # check if string inputs are set
+    expected_input = f"PMID-{pmid_plate}_EXPID-{Experiment_name}-{Experiment_num}_{Lastname}.{Firstname}"
+    textFromDiv = driver.find_element(By.XPATH, "//div[@class='shiny-text-output shiny-bound-output']").text
+    while(textFromDiv!=expected_input):
+        time.sleep(0.2) # sleep-delay
+        
+        # refresh read
+        textFromDiv = driver.find_element(By.XPATH, "//div[@class='shiny-text-output shiny-bound-output']").text
+    
+    # execute action button when all inputs are set
     driver.find_element(By.ID, "do").click()
-    time.sleep(0.5)
-    textFromDiv = driver.find_element(By.XPATH, "//div[@class='shiny-text-output shiny-bound-output']").text
     file_name = "CommandList_" + textFromDiv + ".csv"
     
-    if(simulation== "1"):
-        path_to_cmd = "C://Users//jornb//Documents//GitHub//ot2new//Execution code for OT2//Incubator//OT2DirectprotocolCustomizer//Webdriver//Firefox download test" + '//' + file_name
-    else:
-        path_to_cmd = "C://Users//cvhLa//Onedrive//Desktop//User input (for direct)" + '//' + file_name
-    
-    checkfilepresent=os.path.isfile(path_to_cmd)
-    
-    #This is a recommendation --> it checks if the file already is present, with the input from a few variables before.
-    if(checkfilepresent == True):
-        #If the filename already exists this might lead to problems with the further processing of the file. Also forces people to think about their names (when using the app)
-        sg.Popup("The file name is already taken please change this", keep_on_top=True)
-        driver.close()
-    
-    else:
-        #if not it will download the files
-        driver.find_element(By.ID, "d_OT2").click()
-        time.sleep(1)
-        driver.find_element(By.ID, "guide").click()
-        
-        #checks another time if the command file exists.
-        time.sleep(1)
-        checkdownload = os.path.isfile(path_to_cmd)
-        oldpath = "C://Users//cvhLa//Downloads" + '//' + file_name
-        if (checkdownload == False):
-            try:
-                os.replace(oldpath, path_to_cmd)
-            except:
-                pass
-        
-        RSP = "Robothandler_" + textFromDiv + ".xlsx"
-        
-        if(simulation == "1"):
-            path_to_RSP = "C://Users//jornb//Documents//GitHub//ot2new//Execution code for OT2//Incubator//OT2DirectprotocolCustomizer//Webdriver//Firefox download test" + '//' + RSP
-        else:
-            path_to_RSP = "C://Users//cvhLa//Onedrive//Desktop//User input (for direct)" + '//' + RSP
-        
-        checkdownloadrsp = os.path.isfile(path_to_RSP)
-        
-        if(checkdownload == False):
-            driver.find_element(By.ID, "d_OT2").click()
-            if(simulation == "1"):
-                new_pathRSP = "C://Users//jornb//Downloads//" + RSP
-            else:
-                new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
-
-        elif(checkdownloadrsp == False):
-            driver.find_element(By.ID, "guide").click()
-            if(simulation == "1"):
-                new_pathRSP = "C://Users//jornb//Downloads//" + RSP
-            else:
-                new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
-        
-        else:
-            if(simulation == "1"):
-                new_pathRSP = "C://Users//jornb//Downloads//" + RSP
-            else:
-                new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
-        
-        try:
-            os.replace(path_to_RSP, new_pathRSP)
-        except:
-            pass
-        
-        sg.Popup("Files should be downloaded(click OK when ready to close browser and continue)", keep_on_top = True)
-        
-        x = "CommandList_" + textFromDiv
-        driver.close()
-        #X is the command list so it appears in the Main window
-    return x
-
-#completely the same as the previous file sending with only minor changes
-def Filesending384(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstname, Lastname, Experiment_name, Experiment_num, simulation):
-    x = 'NA'
-    fileinput = driver.find_element(By.ID, "file").send_keys(fullpath)
-    if (fillingrobot == True):
-        element = driver.find_element(By.ID, "fillOuter")
-        element.click()
-    else:
-        print("no outerwell will be filled by OT") 
-    
-    fileinput = driver.find_element(By.ID, "file").send_keys(fullpath)
-    Plate_Map_ID = driver.find_element(By.ID, "pmid").send_keys(pmid_plate)
-    Firstname_file = driver.find_element(By.ID, "f_name").send_keys(Firstname)
-    Lastname_file = driver.find_element(By.ID, "l_name").send_keys(Lastname)
-    Experiment_name_file = driver.find_element(By.ID, "exp_name").send_keys(Experiment_name)
-    Experiment_num_file = driver.find_element(By.ID, "exp_num").send_keys(Experiment_num)
-    
-    #these sleeptimers are so it doesnt just try to download or click something while this is not possible or might give some problems
-    time.sleep(2)
-    confirmupload = driver.find_element(By.ID, "do").click()
-    time.sleep(3)
-    textFromDiv = driver.find_element(By.XPATH, "//div[@class='shiny-text-output shiny-bound-output']").text
-    file_name = "CommandList_" + textFromDiv + ".csv"
-    
+    # path adjustment
     if(simulation == "1"):
         path_to_cmd = "C://Users//jornb//Documents//GitHub//ot2new//Execution code for OT2//Incubator//OT2DirectprotocolCustomizer//Webdriver//Firefox download test" + '//' + file_name
     else:
         path_to_cmd = "C://Users//cvhLa//Onedrive//Desktop//User input (for direct)" + '//' + file_name
     
+    # check if path is valid/present
     checkfilepresent=os.path.isfile(path_to_cmd)
     
     #This is a recommendation --> it checks if the file already is present, with the input from a few variables before.
@@ -201,10 +127,15 @@ def Filesending384(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstnam
         driver.close()
     
     else:
-        #if not it will download the files
-        DownloadRobot = driver.find_element(By.ID, "d_OT2").click()
-        time.sleep(3)
-        DownloadSetup = driver.find_element(By.ID, "guide").click()
+        # wait until download buttons are available, timeout = 20 seconds
+        wait = WebDriverWait(driver, 20)
+        cmdList_download = wait.until(EC.element_to_be_clickable((By.ID, "d_OT2")))
+        usrGuide_download = wait.until(EC.element_to_be_clickable((By.ID, "guide")))
+        
+        # download files
+        cmdList_download.click()
+        usrGuide_download.click()
+        
         #checks another time if the command file exists.
         time.sleep(3)
         checkdownload = os.path.isfile(path_to_cmd)
@@ -214,6 +145,8 @@ def Filesending384(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstnam
                 os.replace(oldpath, path_to_cmd)
             except:
                 pass
+        
+        # RSP? -- userGuide file name
         RSP = "Robothandler_" + textFromDiv + ".xlsx"
         
         if(simulation == "1"):
@@ -222,24 +155,53 @@ def Filesending384(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstnam
             path_to_RSP = "C://Users//cvhLa//Onedrive//Desktop//User input (for direct)" + '//' + RSP
         checkdownloadrsp = os.path.isfile(path_to_RSP)
         
-        if(checkdownload == False):
-            DownloadRobot = driver.find_element(By.ID, "d_OT2").click()
-        elif(checkdownloadrsp == False):
-            DownloadSetup = driver.find_element(By.ID, "guide").click()
-        else:
-            if(simulation == "1"):
-                new_pathRSP = "C://Users//jornb//Downloads//" + RSP
+        # attemot retires?
+        if(mode_384):
+            if(checkdownload == False):
+                driver.find_element(By.ID, "d_OT2").click()
+            elif(checkdownloadrsp == False):
+                driver.find_element(By.ID, "guide").click()
             else:
-                new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
+                if(simulation == "1"):
+                    new_pathRSP = "C://Users//jornb//Downloads//" + RSP
+                else:
+                    new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
+        
+        else:
+            if(checkdownload == False):
+                driver.find_element(By.ID, "d_OT2").click()
+                if(simulation == "1"):
+                    new_pathRSP = "C://Users//jornb//Downloads//" + RSP
+                else:
+                    new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
+    
+            elif(checkdownloadrsp == False):
+                driver.find_element(By.ID, "guide").click()
+                if(simulation == "1"):
+                    new_pathRSP = "C://Users//jornb//Downloads//" + RSP
+                else:
+                    new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
             
+            else:
+                if(simulation == "1"):
+                    new_pathRSP = "C://Users//jornb//Downloads//" + RSP
+                else:
+                    new_pathRSP = "C://Users//cvhLa//Downloads//" + RSP
+        
+       
+        # rename RSP?
         try:
             os.replace(path_to_RSP, new_pathRSP)
         except:
             pass
- 
+        
+        # reports
         sg.Popup("Files should be downloaded(click OK when ready to close browser and continue)", keep_on_top = True)
         x = "CommandList_" + textFromDiv
+        
+        # close webdriver window
         driver.close()
+        
     return x
 
 #Supporting functions
@@ -295,6 +257,7 @@ def popup_connecting():
        window.close() 
     return
 
+#MAIN-----------------------------------
 #Initiation
 simulation, x = prepare()
 window1, window2 = None, None
@@ -302,6 +265,7 @@ window1 = Mainwindow(simulation, x)
 
 #Start window loop
 while True:
+    # initiate window status
     window, event, values = sg.read_all_windows()
     
     #Stops everything when user uses the button cancel or closes the window
@@ -641,7 +605,7 @@ while True:
             assert "MIC - 384 Well Plate.title"
             fillingrobot = values['Fill']
             notfillingrobot = values['nFill']
-            x = Filesending384(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstname, Lastname, Experiment_name, Experiment_num, simulation)
+            x = Filesending(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstname, Lastname, Experiment_name, Experiment_num, simulation, mode_384=True)
         
         elif(values['M9MixR']== True):
             driver = webdriver.Firefox(service = service, options = options)
@@ -661,7 +625,7 @@ while True:
             assert "48 Well Plate.title"
             fillingrobot = values['Fill']
             notfillingrobot = values['nFill']
-            x = Filesending384(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstname, Lastname, Experiment_name, Experiment_num, simulation)    
+            x = Filesending(fullpath, fillingrobot, notfillingrobot, pmid_plate, Firstname, Lastname, Experiment_name, Experiment_num, simulation, mode_384=True)    
         
         else:
             sg.Popup('Please select the method you want to use (the app is going to crash now)')
